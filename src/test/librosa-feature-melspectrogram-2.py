73df71a4c1cabdf29d83a059ad55920b6e26a0e9
ycs1m1yk <satoly4@gmail.com>
2020-03-26 15:01:50 +0900

    Print duration_len

--- src/test/librosa-feature-melspectrogram-2.py
+++ src/test/librosa-feature-melspectrogram-2.py
@@ -9,8 +9,9 @@ import time
 start_time = time.time()
 
 audio_path = "./audio/GRF vs kt - Round 1 Game 1  LCK Spring Split  Griffin vs kt Rolster (2020).mp4"
+duration_len = 60
 
-y, sr = librosa.load(audio_path, duration=60)
+y, sr = librosa.load(audio_path, duration=duration_len)
 librosa.feature.melspectrogram(y=y, sr=sr)
 # array([[  2.891e-07,   2.548e-03, ...,   8.116e-09,   5.633e-09],
 # [  1.986e-07,   1.162e-02, ...,   9.332e-08,   6.716e-09],
