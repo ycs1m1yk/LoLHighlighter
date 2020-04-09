95854799a38baa5393c9aaef1f8141be74d4d98f
YeongHyeonKim <kwxyk@naver.com>
2020-04-09 19:32:20 +0900

    특정 길이만큼 analyze하는 iteration 추가 / rule hit time 저장하는 list 생성

--- src/test/audio_test.py
+++ src/test/audio_test.py
@@ -5,21 +5,44 @@ import matplotlib.pyplot as plt
 
 def audio_test(directory):
     try:
-        # add offset parameter for librosa.load() to specify starting time.
-        # duration parameter is the total time that librosa analyze.
-        y, sr = librosa.load(directory, duration=240)
-        time = np.linspace(0, len(y)/sr, len(y))
+        offset = 0
+        figure_num = 1
+        duration = 360
+        hits = []
 
-        # Exclude general output values ​​to check only special values.
-        adjust = np.where((y > -0.5) & (y < 0.5), 0, y)
+        while True:
+            # add offset parameter for librosa.load() to specify starting time.
+            # duration parameter is the total time that librosa analyze.
+            try:
+                y, sr = librosa.load(directory, offset=offset, duration=duration)
+            except Exception as e:
+                print(e)
+                break
+            time = np.linspace(0, len(y)/sr, len(y))
 
-        # Graph output
-        plt.title(directory)
-        plt.xlabel("Time(sec)")
-        plt.ylabel("Amplitude")
-        plt.plot(time, adjust, c='b', label='waveform')
-        plt.savefig(directory+'.png')
-        plt.show()
+            # Exclude general output values ​​to check only special values.
+            adjust = np.where((y > -0.5) & (y < 0.5), 0, y)
+            for i in range(adjust.size):
+                if adjust[i] != 0:
+                    hit_round = round(time[i] + offset)
+                    hits.append(int(hit_round))
+
+            '''
+            # Graph output
+            plt.title(directory + ", num: " + str(figure_num))
+            plt.xlabel("Time(sec)")
+            plt.ylabel("Amplitude")
+            plt.plot(time, adjust, c='b', label='waveform')
+            plt.savefig(directory + ", num " + str(figure_num) + '.png')
+            plt.show()
+            '''
+            offset += duration
+            figure_num += 1
+            del y
+
+        hits_remove_dup = list(set(hits))
+        hits_remove_dup.sort()
+        print(hits_remove_dup)
 
     except Exception as e:
         print(e)
