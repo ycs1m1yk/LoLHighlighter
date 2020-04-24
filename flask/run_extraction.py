67f4b96643758564d64930e9c4df2c1294a97b79
ycs1m1yk <satoly4@gmail.com>
2020-04-25 00:27:08 +0900

    Check elapsed time

--- flask/run_extraction.py
+++ flask/run_extraction.py
@@ -1,3 +1,5 @@
+import time
+
 from extract import audio_extraction
 from audio_test import audio_test
 
@@ -13,9 +15,12 @@ def run_extraction(url):
         audio_filename = audio_extraction(YouTubeURL)
         directory = './audio/'+audio_filename+'.mp4'
 
+        start = time.time()
         offsets = audio_test(directory)
 
     except Exception as e:
         print(e)
 
+    elapsed_time = time.time() - start
+    print("elapsed time: ", "%.2f" % elapsed_time, "seconds")
     return offsets
