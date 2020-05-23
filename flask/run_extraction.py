f6ab9f93e351ba8276de52a1cba39cbff16f41de
YeongHyeonKim <kwxyk@naver.com>
2020-05-23 16:46:02 +0900

    모듈 실행 순서 변경

--- flask/run_extraction.py
+++ flask/run_extraction.py
@@ -2,24 +2,25 @@ import time
 
 from extract import *
 from audio_test import audio_test
-from video_test import get_game_start_time
+from video_test import *
 
 def run_extraction(url):
-    print(" ------------------------------------------")
-    print("|  Running extraction...")
-    print(" ------------------------------------------")
-
     YouTubeURL = url
+    print(" ------------------------------------------")
+    print(" Running extraction :", YouTubeURL)
+    print(" ------------------------------------------")
 
     try:
+        start = time.time()
         audio_filename = audio_extraction(YouTubeURL)
         video_filename = video_extraction(YouTubeURL)
         audio_dir = './audio/'+audio_filename+'.mp4'
         video_dir = './video/'+video_filename
 
-        start = time.time()
-        offsets = audio_test(audio_dir)
-        get_game_start_time(video_dir)
+
+        game_start_time = get_game_start_time(video_dir)
+        game_end_time = get_game_end_time(video_dir)
+        offsets = audio_test(audio_dir, game_start_time, game_end_time)
 
     except Exception as e:
         print(e)
