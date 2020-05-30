27d35d3cf6be488e2598ae49cff964a7a4bf9450
YeongHyeonKim <kwxyk@naver.com>
2020-05-30 15:48:53 +0900

    리턴 오류 해결

--- flask/video_test.py
+++ flask/video_test.py
@@ -21,7 +21,7 @@ def get_game_start_time(directory):
         vid_cap.set(cv2.CAP_PROP_POS_FRAMES, frame_now)
         ret, image = vid_cap.read()
 
-        time_now, kl, kr = ocr.time_ocr(image)
+        time_now = ocr.time_ocr(image)
         time_split = time_now.split(":")
         digit_count = 0
         for i in time_split:
