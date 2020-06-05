595609b4cb87f2ee494458066fcec9f4e23956c7
YeongHyeonKim <kwxyk@naver.com>
2020-06-06 06:23:41 +0900

    이미지 object list 리턴하는 함수 구현

--- flask/images_from_interval.py
+++ flask/images_from_interval.py
@@ -2,6 +2,7 @@ import cv2
 
 NUM_IMAGE = 5
 
+
 def images_from_interval(start, end, directory):
     vid_cap = cv2.VideoCapture(directory+'.mp4')
 
@@ -26,3 +27,23 @@ def images_from_interval(start, end, directory):
     return img_directories
 
 
+def image_objects_from_interval(start, end, directory):
+    vid_cap = cv2.VideoCapture(directory + '.mp4')
+
+    if not vid_cap.isOpened():
+        print("could not open :", directory, '.mp4')
+        return -1
+    fps = vid_cap.get(cv2.CAP_PROP_FPS)
+    start_frame = int(start * fps)
+    end_frame = int(end * fps)
+    step_size = int((end - start + 1) / NUM_IMAGE * fps)
+
+    images = []
+    for frame in range(start_frame, end_frame, step_size):
+        vid_cap.set(cv2.CAP_PROP_POS_FRAMES, frame)
+        ret, image = vid_cap.read()
+        # test file write for check output
+        images.append(image)
+
+    print('image from intervals')
+    return images
